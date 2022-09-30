3c5aa8ea86e9e071442a63e79ce0c14cd9519dcd
*Nh8qpTcco6 <truong.pham@dirox.net>
2022-09-30 17:37:25 +0700

    add test

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -7,6 +7,8 @@ import renderer from 'react-test-renderer';
 import HomeScreen from '../app/screens/Home';
 import { Provider } from 'react-redux';
 import store from '../app/store';
+import AddEditProductScreen from '../app/screens/AddEditProduct';
+import ProductListScreen from '../app/screens/ProductList';
 
 // it('renders correctly', () => {
 //   renderer.create(<App />);
@@ -20,3 +22,23 @@ describe('test home page', () => {
     )
   })
 })
+
+// describe('test home page', () => {
+//   it('renders the component correctly', () => {
+//     renderer.create(
+//       <Provider store={store}>
+//         <AddEditProductScreen />
+//       </Provider>
+//     )
+//   })
+// })
+
+// describe('test home page', () => {
+//   it('renders the component correctly', () => {
+//     renderer.create(
+//       <Provider store={store}>
+//         <ProductListScreen />
+//       </Provider>
+//     )
+//   })
+// })
